efa49a9488a7bc59d8308a066ad4edae08741404
Arseniy <arseniy.bendyukov2006@gmail.com>
2021-08-21 20:44:01 +0300

    add biult react app

--- my_app_backend/settings.py
+++ my_app_backend/settings.py
@@ -52,7 +52,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+     'http://localhost:3000',
+     'https://arseniyduck.github.io/todo-app-large-build/',
 ]
 
 ROOT_URLCONF = 'my_app_backend.urls'
