0dccf2a8e2f4fe4495727585267d0b8bf74b0b9b
Arseniy <arseniy.bendyukov2006@gmail.com>
2021-08-21 18:58:51 +0300

    debug fix

--- my_app_backend/settings.py
+++ my_app_backend/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
