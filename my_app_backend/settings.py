d1557682c229e3df385a47b82dfdcf9af6fc44b6
Arseniy <arseniy.bendyukov2006@gmail.com>
2021-08-22 16:48:50 +0300

    fix csrf 114

--- my_app_backend/settings.py
+++ my_app_backend/settings.py
@@ -55,9 +55,11 @@ CORS_ORIGIN_WHITELIST = [
    'https://todo-app-large.herokuapp.com',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://todo-app-large.herokuapp.com',]
+CSRF_TRUSTED_ORIGINS = ['todo-app-large.herokuapp.com',]
 
 # CSRF_COOKIE_SECURE = True
 # SESSION_COOKIE_SECURE = True
